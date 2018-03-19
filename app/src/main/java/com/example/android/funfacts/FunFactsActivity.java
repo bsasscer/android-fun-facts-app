996959f90d7e47e8e3286d9d7e074508999bf1fa
bsasscer <smithbi.maricela@gmail.com>
2018-03-19 09:11:41 -0700

    Make layout, style changes, and add TextView and Button fields

--- app/src/main/java/com/example/android/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/android/funfacts/FunFactsActivity.java
@@ -2,8 +2,14 @@ package com.example.android.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+    // Declare our View variables
+    private TextView factTextView;
+    private Button showFactButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
