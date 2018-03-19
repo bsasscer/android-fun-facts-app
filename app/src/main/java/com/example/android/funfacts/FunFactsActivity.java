4edd24244485bfa55a64451b86e7dd07a721da1d
bsasscer <smithbi.maricela@gmail.com>
2018-03-19 13:02:04 -0700

    Add log util

--- app/src/main/java/com/example/android/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/android/funfacts/FunFactsActivity.java
@@ -3,14 +3,17 @@ package com.example.android.funfacts;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
     private FactBook factBook = new FactBook();
     private ColorWheel colorWheel = new ColorWheel();
     // Declare our View variables
@@ -45,5 +48,8 @@ public class FunFactsActivity extends AppCompatActivity {
             }
         };
         showFactButton.setOnClickListener(listener);
+
+        //Toast.makeText(this, "Yay! Our activity was created!", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "We're logging from the onCreate() method!");
     }
 }
