f83512e94da57e58c913ac97e6998bf2b53c3e32
bsasscer <smithbi.maricela@gmail.com>
2018-03-19 09:29:21 -0700

    Add OnClickListener to showFactButton

--- app/src/main/java/com/example/android/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/android/funfacts/FunFactsActivity.java
@@ -2,6 +2,7 @@ package com.example.android.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,5 +16,18 @@ public class FunFactsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+
+        // Assign the Views from the layout file to the corresponding variables
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 }
