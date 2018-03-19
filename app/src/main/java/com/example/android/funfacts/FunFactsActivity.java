1f44540be8877a0a6534de353574d3ff3e893013
bsasscer <smithbi.maricela@gmail.com>
2018-03-19 10:35:53 -0700

    Create ColorWheel class and getColor method to randomly select different colors for the background

--- app/src/main/java/com/example/android/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/android/funfacts/FunFactsActivity.java
@@ -1,18 +1,22 @@
 package com.example.android.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
     private FactBook factBook = new FactBook();
+    private ColorWheel colorWheel = new ColorWheel();
     // Declare our View variables
     private TextView factTextView;
     private Button showFactButton;
+    private RelativeLayout relativeLayout;
 
 
     @Override
@@ -24,6 +28,7 @@ public class FunFactsActivity extends AppCompatActivity {
         // Assign the Views from the layout file to the corresponding variables
         factTextView = findViewById(R.id.factTextView);
         showFactButton = findViewById(R.id.showFactButton);
+        relativeLayout = findViewById(R.id.relativeLayout);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
@@ -32,6 +37,10 @@ public class FunFactsActivity extends AppCompatActivity {
 
                 // Update the screen with our new fact
                 factTextView.setText(fact);
+
+                int color = colorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
 
             }
         };
